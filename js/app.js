cee994e189e356ac19968b7734a478b608f73586
Chandu-Thota <chanduthota2429@gmail.com>
2020-07-02 18:20:16 +0530

    module1 solution

---
+++ js/app.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController ($scope) {
+    $scope.items = ""
+    $scope.message = "";
+    $scope.itemscheck = function() {
+      $scope.items = $scope.items.trim();
+
+      if ($scope.items === "") {
+        $scope.message = "Please enter data first.";
+        $scope.noticeType = "danger";
+      }
+      else {
+        var $itemsArray = $scope.items.split(",");
+        var $length = $itemsArray.length;
+        if ( ($length <= 3) || ($length == 4 && $itemsArray[3].trim() === "") ) {
+          $scope.message = "Enjoy!";
+          $scope.noticeType = "success";
+        }
+        else {
+          $scope.message = "Too much!";
+          $scope.noticeType = "warning";
+        }
+      }
+    };
+  }
+})();
